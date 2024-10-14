13f78e899ba1678a606e744322b789e776260eb3
kadir-zeren <k4dzeren@gmail.com>
2024-10-14 20:51:26 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,33 @@
+text = 'Clarusway'
+text[0]
+
+text[: 5] + 'k' + text[-3:]
+
+yeni_string = text[:5] + 'k' + text[-3:]
+
+var_string = 'ClarusWay'
+print(var_string.lower())
+print(var_string)
+var_string = 'ClarusWay' . lower()
+print(var_string)
+
+text = 'AaBbCc'
+print(text.lower())
+
+text = text. lower()
+
+var_str = 'In God we Trust'
+var_str.lower()
+print(var_str)
+
+cumle = 'In God We Trust'
+cumle.lower()
+print(cumle)
+
+yeni_cumle = cumle. lower()
+cumle = cumle. lower()
+
+e_mail = 'bulent@clarusway.com'
+print ('@' in e_mail)
+
+'t' in cumle
